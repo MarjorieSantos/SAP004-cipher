228d294e91e638bf51fa720d2d1a6ab664ab15f8
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-01 16:46:24 -0300

    primeiro-commit-ceaser-cipher

--- src/index.js
+++ src/index.js
@@ -1,3 +1,32 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+let message = document.getElementById("msg")
+let offset = document.getElementById("offset")
+let choice = document.getElementById("choice")
+let choice2 = document.getElementById("choice2")
+let result = document.getElementById("result")
+let result_name = document.getElementById("result-name")
+
+choice.onclick = function(){
+        if (message.value == "" || offset.value == ""){
+            alert("Por favor, preencha o campo requerido")
+        } else {
+            result_name.innerHTML = cipher.encode(offset.value, message.value)
+            result.style.display = "block"
+        }
+}
+
+choice2.onclick = function (){
+    if (message.value == "" || offset.value == ""){
+        alert("Por favor, preencha o campo requerido")
+    } else {
+        result_name.innerHTML = cipher.decode(offset.value, message.value)
+        result.style.display = "block"
+    }
+}
+
+
+
+
+
+
