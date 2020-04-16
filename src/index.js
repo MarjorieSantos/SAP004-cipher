ed2720c86c915435206d6336c054cecad69e34ab
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-16 19:54:31 -0300

    inclusao-css-no-js

--- src/index.js
+++ src/index.js
@@ -1,46 +1,52 @@
 
 import cipher from './cipher.js';
 
-const btn_back = document.getElementById("btn-back")
-const page1 = document.getElementById("page1")
-const page2 = document.getElementById("page2")
-const btn_inicio = document.getElementById("btn-start")
-const message = document.getElementById("msg")
-const offset = document.getElementById("offset")
-const choice = document.getElementById("choice")
-const choice2 = document.getElementById("choice2")
-const result = document.getElementById("result")
-const result_name = document.getElementById("result-name")
+const btn_back = document.querySelector(".js-btn-back")
+const page1 = document.querySelector("#page1")
+const page2 = document.querySelector("#page2")
+const btn_inicio = document.querySelector(".js-btn-start")
+const message = document.querySelector(".js-msg")
+const offset = document.querySelector(".js-offset")
+const choice = document.querySelector(".js-encode")
+const choice2 = document.querySelector(".js-decode")
+const result = document.querySelector(".js-result")
+const result_name = document.querySelector("#result-name")
 
-btn_inicio.onclick = function(){
-    page1.style.display = "none"
-    page2.style.display = "block"
-    btn_back.style.display = "block"
-}
+btn_inicio.addEventListener("click", function () {
+    page1.classList.add("hidden-content")
+    page2.classList.add("show-content")
+    btn_back.classList.add("show-content")
+    result.classList.add("hidden-content")
+    result.classList.remove("show-content")
+})
 
-btn_back.onclick = function(){
-    page1.style.display = "block"
-    page2.style.display = "none"
-    btn_back.style.display = "none"
+btn_back.addEventListener("click", function () {
+    page1.classList.remove("hidden-content")
+    page2.classList.add("hidden-content")
+    page2.classList.remove("show-content")
+    btn_back.classList.add("hidden-content")
+    btn_back.classList.remove("show-content")
+    result.classList.add("hidden-content")
     message.value = ""
     offset.value = ""
-    result.style.display = "none"
-}
+})
 
-choice.onclick = function(){
-        if (message.value == "" || offset.value == ""){
-            alert("Por favor, preencha o campo requerido")
-        } else {
-            result_name.innerHTML = cipher.encode(offset.value, message.value)
-            result.style.display = "block"
-        }
-}
-
-choice2.onclick = function (){
-    if (message.value == "" || offset.value == ""){
+choice.addEventListener("click", function () {
+    if (message.value == "" || offset.value == "") {
         alert("Por favor, preencha o campo requerido")
     } else {
-        result_name.innerHTML = cipher.decode(offset.value, message.value)
-        result.style.display = "block"
+        result_name.innerHTML = cipher.encode(offset.value, message.value)
+        result.classList.add("show-content")
+        result.classList.remove("hidden-content")
     }
-}
+})
+
+choice2.addEventListener("click", function () {
+    if (message.value == "" || offset.value == "") {
+        alert("Por favor, preencha o campo requerido")
+    } else {
+        result_name.textContent = cipher.decode(offset.value, message.value)
+        result.classList.add("show-content")
+        result.classList.remove("hidden-content")
+    }
+})
