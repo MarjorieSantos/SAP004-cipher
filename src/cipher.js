1295e0839120ff487847827c8a1bd26a0eb2d5fb
Marjorie Santos Coutinho Ferreira <marjorie.2001@hotmail.com>
2020-04-16 11:05:11 -0300

    primeira-alteracao-branch

--- src/cipher.js
+++ src/cipher.js
@@ -1,5 +1,36 @@
 const cipher = {
-  // ...
+  encode: function (offset, string) {
+    const novaPalavra = ""; //ela recebe uma string vazia para poder concatenar com a palavra gerada pelo novo charCode
+    const palavraMaiuscula = string.toUpperCase();
+    for (let contador = 0; contador < palavraMaiuscula.length; contador++) {
+      const novaLetra = palavraMaiuscula[contador]
+      if(novaLetra !== " "){
+      const charCodeLetraAtual = novaLetra.charCodeAt();
+      const novoCharCode = ((charCodeLetraAtual - 65 + parseInt(offset)) % 26) + 65;
+      novaLetra = String.fromCharCode(novoCharCode);
+      }
+      novaPalavra = novaPalavra + novaLetra
+
+    }
+    return novaPalavra
+  },
+
+  decode: function (offset, string) {
+    const novaPalavra = ""
+    const palavraMaiuscula = string.toUpperCase();
+    for (let contador = 0; contador < palavraMaiuscula.length; contador++) {
+      const novaLetra = palavraMaiuscula[contador]
+      if(novaLetra !== " "){
+      const charCodeLetraAtual = palavraMaiuscula[contador].charCodeAt();
+      const novoCharCode = ((charCodeLetraAtual + 65 - parseInt(offset)) % 26) + 65;
+      novaLetra = String.fromCharCode(novoCharCode);
+      }
+      
+      novaPalavra = novaPalavra + novaLetra
+
+    }
+    return novaPalavra
+  }
 };
 
 export default cipher;
--- src/index.js
+++ src/index.js
@@ -1,3 +1,46 @@
+
 import cipher from './cipher.js';
 
-console.log(cipher);
+const btn_back = document.getElementById("btn-back")
+const page1 = document.getElementById("page1")
+const page2 = document.getElementById("page2")
+const btn_inicio = document.getElementById("btn-start")
+const message = document.getElementById("msg")
+const offset = document.getElementById("offset")
+const choice = document.getElementById("choice")
+const choice2 = document.getElementById("choice2")
+const result = document.getElementById("result")
+const result_name = document.getElementById("result-name")
+
+btn_inicio.onclick = function(){
+    page1.style.display = "none"
+    page2.style.display = "block"
+    btn_back.style.display = "block"
+}
+
+btn_back.onclick = function(){
+    page1.style.display = "block"
+    page2.style.display = "none"
+    btn_back.style.display = "none"
+    message.value = ""
+    offset.value = ""
+    result.style.display = "none"
+}
+
+choice.onclick = function(){
+        if (message.value == "" || offset.value == ""){
+            alert("Por favor, preencha o campo requerido")
+        } else {
+            result_name.innerHTML = cipher.encode(offset.value, message.value)
+            result.style.display = "block"
+        }
+}
+
+choice2.onclick = function (){
+    if (message.value == "" || offset.value == ""){
+        alert("Por favor, preencha o campo requerido")
+    } else {
+        result_name.innerHTML = cipher.decode(offset.value, message.value)
+        result.style.display = "block"
+    }
+}
